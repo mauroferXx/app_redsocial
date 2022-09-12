b0b05e6bd3185e61602ad3c3c0c8f2f8e16fa463
Aniuska <an.ojeda@duocuc.cl>
2022-09-12 20:41:14 -0300

    Cambios configuraciones

--- src/app/configuraciones/configuraciones.page.ts
+++ src/app/configuraciones/configuraciones.page.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-configuraciones',
   templateUrl: './configuraciones.page.html',
   styleUrls: ['./configuraciones.page.scss'],
 })
-export class ConfiguracionesPage implements OnInit {
+export class ConfiguracionesPage{
 
-  constructor() { }
+  handlerMessage = '';
+  roleMessage = '';
 
-  ngOnInit() {
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: '¿Quieres salir de Instagram?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.handlerMessage = 'Alert canceled';
+          },
+        },
+        {
+          text: 'Confirmar',
+          role: 'confirm',
+          handler: () => {
+            this.handlerMessage = 'Alert confirmed';
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `Dismissed with role: ${role}`;
   }
 
 }
